d729e63223e448b1e9fbebefbe583ccbf47cdfe5
Bilal Bhatti <bilal.bhatti@ca.com>
2017-04-18 00:43:57 -0500

    WIP: usb-to-usb converter

---
+++ usb_to_usb/keymap_spacefn.c
@@ -0,0 +1,63 @@
+  /*
+ * SpaceFN layout
+ * http://geekhack.org/index.php?topic=51069.0
+ */
+#include "keymap_common.h"
+
+
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: default
+     * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
+     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau|
+     * `---'   `---------------' `---------------' `---------------' `-----------'
+     * ,-----------------------------------------------------------. ,-----------.
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backspa| |Ins|Hom|PgU|
+     * |-----------------------------------------------------------| |-----------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \| |Del|End|PgD|
+     * |-----------------------------------------------------------| `-----------'
+     * |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
+     * |-----------------------------------------------------------|     ,---.
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shift     |     |Up |
+     * |-----------------------------------------------------------| ,-----------.
+     * |Ctrl |Alt |Gui |         Space         |Gui |Alt |Menu|Ctrl| |Lef|Dow|Rig|
+     * `-----------------------------------------------------------' `-----------'
+     */
+    [0] = KEYMAP(
+    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,           PSCR,SLCK,BRK,
+    GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS, HOME,PGUP,    TRNS,TRNS,TRNS,TRNS,
+    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL, END, PGDN,    TRNS,TRNS,TRNS,
+    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         TRNS,TRNS,TRNS,TRNS,
+    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,          UP,           TRNS,TRNS,TRNS,
+    LCTL,LALT,LGUI,          FN0,                     RGUI,RALT,APP, RCTL,     LEFT,DOWN,RGHT,    TRNS,TRNS,TRNS
+    ),
+
+    /* 1: SpaceFN
+     * ,---.  ,-----------.  ,---------------. ,-------------------.
+     * |   |  |  |  |  |  |  |  |  |Prev|Play| |Next|Mute|Vold|Volu|
+     * `---'  `-----------'  `---------------' `-------------------'
+     * ,-----------------------------------------------------------.
+     * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
+     * |-----------------------------------------------------------|
+     * |     |   |   |Esc|   |   |   |Hom|Up |End|Psc|Slk|Pau|Ins  |
+     * |-----------------------------------------------------------|
+     * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
+     * |-----------------------------------------------------------|      ,----.
+     * |        |   |   |   |   |Spc|PgD|`  |~  |   |Men|          |      |Volu|
+     * |-----------------------------------------------------------| ,--------------.
+     * |    |    |    |                        |    |    |    |    | |Mply|Vold|Mnxt|
+     * `-----------------------------------------------------------' `--------------'
+     */
+    [1] = KEYMAP(
+    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,          PSCR,SLCK,BRK,
+    TRNS,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,      INS, HOME,PGUP,    TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,VOLU, TRNS,TRNS,TRNS,HOME,UP,  END, PSCR,SLCK,PAUS,INS,      DEL, END, PGDN,    TRNS,TRNS,TRNS,
+    TRNS,TRNS,MPLY,VOLD,MNXT,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,     TRNS,                        TRNS,TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,GRV, FN1, TRNS,APP,           TRNS,          VOLU,         TRNS,TRNS,TRNS,
+    TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS,     MPLY,VOLD,MNXT,    TRNS,TRNS,TRNS
+    ),
+};
+
+const action_t fn_actions[] = {
+    [0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),
+    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde
+};
